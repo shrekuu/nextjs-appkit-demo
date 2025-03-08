4a0eb8d55c53f804f9ad8090760435b7f634f046
Bond <shrekuu@gmail.com>
2025-03-08 14:39:49 +0800

    chore: suppress build warning

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['pino-pretty', 'lokijs', 'encoding'],
+  serverExternalPackages: ["pino-pretty", "lokijs", "encoding"],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    // ref: https://github.com/WalletConnect/walletconnect-monorepo/issues/1908#issuecomment-1487801131
+    // walletconnect: 'pino-pretty', 'lokijs', 'encoding' are not supported in the browser
+    config.externals = [...config.externals, "pino-pretty", "lokijs", "encoding"];
+
+    return config;
+  },
   eslint: {
     // disable linting during build phase
     ignoreDuringBuilds: true,
