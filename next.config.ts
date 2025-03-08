c4ca75f2d996c81bd5409fe8dfa18aeb4efcf616
Bond <shrekuu@gmail.com>
2025-03-08 14:28:34 +0800

    feat: add walletconnect appkit demo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['pino-pretty', 'lokijs', 'encoding'],
+  eslint: {
+    // disable linting during build phase
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // disable type-checking during build phase
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
---
+++ src/wagmi/provider.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { wagmiAdapter, projectId, networks } from "./config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAppKit } from "@reown/appkit/react";
+import React, { type ReactNode } from "react";
+import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
+import { DefaultSIWX } from "@reown/appkit-siwx";
+
+// Set up queryClient
+const queryClient = new QueryClient();
+
+// Set up metadata
+const metadata = {
+  name: "nextjs-appkit-demo",
+  description: "AppKit Example",
+  url: "https://nextjs-appkit-demo.projects.linwise.com", // origin must match your domain & subdomain
+  icons: ["https://assets.reown.com/reown-profile-pic.png"],
+};
+
+// Create the modal
+export const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks,
+  metadata,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+    send: false,
+    swaps: false,
+    onramp: false,
+    history: false,
+    connectMethodsOrder: ["wallet"],
+  },
+  featuredWalletIds: [
+    "971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709", // okx wallet
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // metamask wallet
+    "8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4", // binance wallet
+  ],
+  excludeWalletIds: [
+    // "971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709", // okx wallet
+    // "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // metamask wallet
+    // "8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4", // binance wallet
+  ],
+  enableWalletConnect: true,
+  allWallets: "SHOW", // "SHOW" | "HIDE" | "ONLY_MOBILE"
+  termsConditionsUrl: "/legal/terms-of-service",
+  privacyPolicyUrl: "/legal/privacy-policy",
+  themeVariables: {
+    "--w3m-accent": "#000000",
+  },
+  siwx: new DefaultSIWX(),
+  // debug: true,
+});
+
+function ContextProvider({ children, cookies }: { children: ReactNode; cookies: string | null }) {
+  const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig as Config, cookies);
+
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig as Config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default ContextProvider;
